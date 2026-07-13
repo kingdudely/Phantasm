671024cd7ede43e58d804cc0ff0661360752a3b3
secret <132377436+secretisadev@users.noreply.github.com>
2026-07-13 23:50:04 +1000

    Update TSB.lua

--- Games/TSB.lua
+++ Games/TSB.lua
@@ -1,2 +1,2 @@
 queueonteleport('loadstring(game:HttpGet("https://github.com/secretisadev/Phantasm/raw/refs/heads/main/Loader.lua"))()')
-game:GetService("TeleportService"):TeleportToPlaceInstance(130818724007978, "c33a9e66-f172-40e0-ab34-093d68049d9e", game:GetService("Players").LocalPlayer)
+game:GetService("TeleportService"):Teleport(130818724007978, game:GetService("Players").LocalPlayer)
